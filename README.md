f74c91e34117deead3c55023480dfb4476efeffd
Raydberg Gabriel Chuquival Gil <raydga_2005@hotmail.com>
2025-01-07 14:17:21 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Welcome to your Expo app 👋
+
---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native"
+
+
+const RootLayout = () => {
+    return (
+
+        <View>
+            <Text>
+                Welcome Navigation App
+            </Text>
+        </View>
+
+    )
+}
+export default RootLayout
